1c49e33019e266cf8373448d71c2741199b4a988
Miki <kim.hszd@gmail.com>
2013-10-26 00:14:57 -0600

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trdemo::Application.config.secret_token = '6bb4d2ff471c7b9cb86ae14b87a392a3dbe2029a4c4b0525e37c0ddfe1751f7e130ad7c35f8d3bbca0811ece74d4a0eb92fb032d2362535115e0e7f11dc26ef9'
